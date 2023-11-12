ce4f69f6c6e0474996417af94290c0775ae8bd8c
Roland Wang <rolandwang@utexas.edu>
2023-11-12 16:54:37 -0600

    cells.h variables and functions

--- Core/Inc/cells.h
+++ Core/Inc/cells.h
@@ -6,13 +6,16 @@
 #define HVC_FIRMWARE_2024_CELLS_H
 
 // Variables
-static float tempData[20];
+static float voltageData[140]; // 5 segments, 28 cells per segment
+static float tempData[91]; // 65% of cells are covered
 static float minTemp;
 static float maxTemp;
 
+
 // Functions
-void processTempData(); // creating spi request, storing data into array
-void processVoltageData(); // creating spi request, storing data into array
-void sendPackData();
+void cellsPeriodic(); // creating spi request, store data, choose when to send temp & voltage packets
+
+static void sendTempPackets(); // 12 packets, 1 byte per temp (int)
+static void sendVoltagePackets(); // 35 packets, 2 bytes per voltage (int)
 
 #endif //HVC_FIRMWARE_2024_CELLS_H
