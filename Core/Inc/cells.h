5d523c777681ec0dcd5cc0ffffd8a04302fadcac
Roland Wang <rolandwang@utexas.edu>
2023-11-12 17:31:36 -0600

    cells.cpp cellsPeriodic function

--- Core/Inc/cells.h
+++ Core/Inc/cells.h
@@ -6,14 +6,14 @@
 #define HVC_FIRMWARE_2024_CELLS_H
 
 // Variables
-static float voltageData[140]; // 5 segments, 28 cells per segment
+static float voltageData[140]; // 5 segments, 28 cells per segment, volts in mV
 static float tempData[91]; // 65% of cells are covered
 static float minTemp;
 static float maxTemp;
 
 
 // Functions
-void cellsPeriodic(); // creating spi request, store data, choose when to send temp & voltage packets
+void cellsPeriodic();
 
 static void sendTempPackets(); // 12 packets, 1 byte per temp (int)
 static void sendVoltagePackets(); // 35 packets, 2 bytes per voltage (int)
--- Core/Src/cells.cpp
+++ Core/Src/cells.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "cells.h"
+
+/** creating spi request, store data, choose when to send temp & voltage packets
+ * find min max temps after low pass filter
+ **/
+void cellsPeriodic() {
+    // Create SPI inbox request
+
+    // Process Temperature Data
+    for(int i = 0 ; i < sizeof(tempData) ; i++) {
+        // store data
+
+        // check for min and max temp of each data point
+        float temp = tempData[i];
+        if(maxTemp < temp) maxTemp = temp;
+        if(minTemp > temp) minTemp = temp;
+    }
+
+    // Process Voltage Data
+    for(int i = 0 ; i < sizeof(voltageData) ; i++) {
+        // store data
+
+    }
+}
