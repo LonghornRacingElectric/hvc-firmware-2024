d0350a79e5ea278773eeb69c310c9abbd74da09e
Roland Wang <rolandwang@utexas.edu>
2023-11-12 16:25:32 -0600

    static variables

--- Core/Inc/cells.h
+++ Core/Inc/cells.h
@@ -5,7 +5,14 @@
 #ifndef HVC_FIRMWARE_2024_CELLS_H
 #define HVC_FIRMWARE_2024_CELLS_H
 
+// Variables
+static float tempData[20];
+static float minTemp;
+static float maxTemp;
 
 // Functions
+void processTempData(); // creating spi request, storing data into array
+void processVoltageData(); // creating spi request, storing data into array
+void sendPackData();
 
 #endif //HVC_FIRMWARE_2024_CELLS_H
--- Core/Inc/thermal.h
+++ Core/Inc/thermal.h
@@ -5,8 +5,8 @@
 #ifndef HVC_FIRMWARE_2024_COOLING_H
 #define HVC_FIRMWARE_2024_COOLING_H
 
-float minTemp = 0.0f;
-float maxTemp = 60.0f;
+static float minTemp = 0.0f;
+static float maxTemp = 60.0f;
 
 void updateParameters();
 bool isTempWithinBounds();
