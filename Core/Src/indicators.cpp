390f4a65c8b2b9289c57468114d729074db5ab39
Roland Wang <rolandwang@utexas.edu>
2023-11-12 18:40:25 -0600

    indicator.cpp DONE

--- Core/Src/indicators.cpp
+++ Core/Src/indicators.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "indicators.h"
+#include "stm32h7xx_hal.h"
+#include "can.h"
+
+void setIndicatorLights(bool amsIndicator, bool imdIndicator)
+{
+    // Runs code every 100 cycles
+    static int i = 0;
+    if(++i < 100) return;
+    i = 0;
+
+    uint8_t data[2] = {amsIndicator, imdIndicator};
+    can_send(HVC_VCU_AMS_IMD, 2, data);
+}
