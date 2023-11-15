e76001b9b7f53546d0769ee5477f443e6b0ea13b
Roland Wang <rolandwang@utexas.edu>
2023-11-14 20:49:35 -0600

    fixed contactors.cpp

--- Core/Src/contactors.cpp
+++ Core/Src/contactors.cpp
@@ -6,30 +6,14 @@
 #include "main.h"
 
 void setPrechargeContactor(bool on) {
-    if(on) {
-        HAL_GPIO_WritePin(Precharge_Enable_GPIO_Port, Precharge_Enable_Pin, GPIO_PIN_SET);
-    }
-    else {
-        HAL_GPIO_WritePin(Precharge_Enable_GPIO_Port, Precharge_Enable_Pin, GPIO_PIN_RESET);
-    }
+    HAL_GPIO_WritePin(Precharge_Enable_GPIO_Port, Precharge_Enable_Pin, (GPIO_PinState) on);
 }
 
 void setDriveContactor(bool on) {
-    if(on) {
-        HAL_GPIO_WritePin(Close_HV__Signal_GPIO_Port, Close_HV__Signal_Pin, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(Close_HV__SignalC5_GPIO_Port, Close_HV__SignalC5_Pin, GPIO_PIN_SET);
-    }
-    else {
-        HAL_GPIO_WritePin(Close_HV__Signal_GPIO_Port, Close_HV__Signal_Pin, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(Close_HV__SignalC5_GPIO_Port, Close_HV__SignalC5_Pin, GPIO_PIN_RESET);
-    }
+    HAL_GPIO_WritePin(Close_HV__Signal_GPIO_Port, Close_HV__Signal_Pin, (GPIO_PinState) on);
+    HAL_GPIO_WritePin(Close_HV__SignalC5_GPIO_Port, Close_HV__SignalC5_Pin, (GPIO_PinState) on);
 }
 
 void setDischargeDisable(bool disable) {
-    if(disable) {
-        HAL_GPIO_WritePin(Discharge_Disable_GPIO_Port, Discharge_Disable_Pin, GPIO_PIN_RESET);
-    }
-    else {
-        HAL_GPIO_WritePin(Discharge_Disable_GPIO_Port, Discharge_Disable_Pin, GPIO_PIN_SET);
-    }
+    HAL_GPIO_WritePin(Discharge_Disable_GPIO_Port, Discharge_Disable_Pin, (GPIO_PinState) disable);
 }
