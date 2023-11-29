002d8ead04f1a94724ccc015f9e95dd9458f006a
angelasrsh <angelasrsh@gmail.com>
2023-11-28 21:18:23 -0600

    added get pack current

--- Core/Src/isense.cpp
+++ Core/Src/isense.cpp
@@ -3,11 +3,17 @@
 //
 
 #include "isense.h"
+#include "adc.h"
 
 bool isPackCurrentWithinBounds() {
 
 }
 
 float getPackCurrent() {
+    float lowCurr = getA_HV_lowCurr_to_MCU(); // set value
+    float highCurr = getA_HV_highCurr_to_MCU();
+
+
 
 }
+
