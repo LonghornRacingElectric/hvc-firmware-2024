d7e6b75df6544a7008c7a9b04be25a4725c5e628
Roland Wang <rolandwang@utexas.edu>
2023-12-10 12:59:26 -0600

    vcu periodic function implementation

--- Core/Src/vcu.cpp
+++ Core/Src/vcu.cpp
@@ -1,16 +1,42 @@
-//
-// Created by rolandwang on 11/12/2023.
-//
 
 #include "vcu.h"
 #include "angel_can.h"
+#include "isense.h"
+#include "vsense.h"
+#include "cells.h"
+#include "imu.h"
+#include "clock.h"
 
 static CanRx parameterMailbox;
+static uint8_t packData[7];
+static uint8_t imuAccel[6];
+static uint8_t imuGyro[6];
 
 void vcuInit() {
     can_addMailbox(VCU_HVC_PARAMS, &parameterMailbox);
 }
 
+/**
+ *  Get pack current, voltage, SoC, max temp -> store in data array (2 bytes each)
+ *  Get imu accel and gyro data
+ * */
 void vcuPeriodic() {
-    // TODO implement
+    can_writeBytes(packData, 0, 2, (uint16_t) (getPackVoltage() / 0.001f));
+    can_writeBytes(packData, 2, 4, (uint16_t) (getPackCurrent() / 0.001f));
+    can_writeBytes(packData, 4, 6, (uint16_t) (getSoC() / 0.001f));
+    can_writeBytes(packData, 6, 7, (uint8_t) getMaxTemp());
+
+    can_writeBytes(imuAccel, 0, 2, (uint16_t) (imu_getAccel()));
+    can_writeBytes(imuAccel, 2, 4, (uint16_t) (imu_getAccel()));
+    can_writeBytes(imuAccel, 4, 6, (uint16_t) (imu_getAccel()));
+
+    can_writeBytes(imuGyro, 0, 2, (uint16_t) (imu_getGyro()));
+    can_writeBytes(imuGyro, 2, 4, (uint16_t) (imu_getGyro()));
+    can_writeBytes(imuGyro, 4, 6, (uint16_t) (imu_getGyro()));
+
+    if(clock_getDeltaTime() == .01f) {
+        can_send(HVC_VCU_PACK_STATUS, 7, packData);
+        can_send(HVC_VCU_IMU_ACCEL, 6, imuAccel);
+        can_send(HVC_VCU_IMU_GYRO, 6, imuGyro);
+    }
 }
