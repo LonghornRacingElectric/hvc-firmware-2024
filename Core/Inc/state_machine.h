b0b0fc7c9fb88c2f1391c1995e1293c08415e01c
Roland Wang <rolandwang@utexas.edu>
2023-12-13 18:20:57 -0600

    Fan control updates

--- Core/Inc/state_machine.h
+++ Core/Inc/state_machine.h
@@ -5,10 +5,11 @@
 #ifndef HVC_FIRMWARE_2024_STATE_MACHINE_H
 #define HVC_FIRMWARE_2024_STATE_MACHINE_H
 
-#define STATE_NOT_ENERGIZED 1
-#define STATE_PRECHARGING 2
-#define STATE_ENERGIZED 3
-#define STATE_CHARGING 4
+#define STATE_NOT_ENERGIZED 1 // idle mode, if PGood signal -> 2
+#define STATE_START_PRECHARGE 2 // sample car voltage, once done -> 3
+#define STATE_PRECHARGING 3 // set contactors, once current V > 0.9 sample V -> 4
+#define STATE_ENERGIZED 4 // disable pcharge, enable drive contactors, if shutdown -> 1
+#define STATE_CHARGING 5
 
 int updateStateMachine(bool hvOk, bool chargerPresent);
 
