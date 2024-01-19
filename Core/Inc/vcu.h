1206904369d36ae9d3d33f024c9ec5e48f8a6a75
Yash Mittal <yashkmittal@utexas.edu>
2024-01-19 17:01:35 -0600

    improv: added can-comms for imu data
    * Send accel and gyro data (multiplied by 100 for .01 precision)
    
    * Updated main to reflect this change to Longhorn Library

--- Core/Inc/vcu.h
+++ Core/Inc/vcu.h
@@ -1,14 +1,24 @@
-//
-// Created by rolandwang on 11/12/2023.
-//
-
 #ifndef HVC_FIRMWARE_2024_VCU_H
 #define HVC_FIRMWARE_2024_VCU_H
 
+/**
+ * Initialize CAN communication with vcu.
+ */
 void vcuInit();
 
 /**
- * gather IMU and pack data, send packets periodically
+ * Send IMU data over CAN. Precision is 0.01 m/s^2 for accel and 0.01 rad/s.
+ * @param accel_x
+ * @param accel_y
+ * @param accel_z
+ * @param gyro_x
+ * @param gyro_y
+ * @param gyro_z
+ */
+void vcuSendImu(float accel_x, float accel_y, float accel_z, float gyro_x, float gyro_y, float gyro_z);
+
+/**
+ * Get status data from VCU.
  */
 void vcuPeriodic();
 
--- Core/Src/vcu.cpp
+++ Core/Src/vcu.cpp
@@ -1,16 +1,33 @@
-//
-// Created by rolandwang on 11/12/2023.
-//
-
 #include "vcu.h"
 #include "angel_can.h"
 
-static CanRx parameterMailbox;
+static CanInbox parameterInbox;
+static CanOutbox imuAccelOutbox;
+static CanOutbox imuGyroOutbox;
 
 void vcuInit() {
-    can_addMailbox(VCU_HVC_PARAMS, &parameterMailbox);
+    can_addInbox(VCU_HVC_PARAMS, &parameterInbox);
+    can_addOutbox(HVC_VCU_IMU_ACCEL, 0.250f, &imuAccelOutbox);
+    can_addOutbox(HVC_VCU_IMU_GYRO, 0.250f, &imuGyroOutbox);
 }
 
-void vcuPeriodic() {
+void vcuSendImu(float accel_x, float accel_y, float accel_z, float gyro_x, float gyro_y, float gyro_z){
+    can_writeBytes(imuAccelOutbox.data, 0, 1, (int16_t) (accel_x * 100));
+    can_writeBytes(imuAccelOutbox.data, 2, 3, (int16_t) (accel_y * 100));
+    can_writeBytes(imuAccelOutbox.data, 4, 5, (int16_t) (accel_z * 100));
+    imuAccelOutbox.isRecent = true;
+
+    can_writeBytes(imuGyroOutbox.data, 0, 1, (int16_t) (gyro_x * 100));
+    can_writeBytes(imuGyroOutbox.data, 2, 3, (int16_t) (gyro_y * 100));
+    can_writeBytes(imuGyroOutbox.data, 4, 5, (int16_t) (gyro_z * 100));
+    imuGyroOutbox.isRecent = true;
+}
+
+void vcuPeriodic(){
     // TODO implement
+    vcuSendImu(0, 0, 0, 0, 0, 0);
+    if(parameterInbox.isRecent){
+        parameterInbox.isRecent = false;
+        // TODO implement
+    }
 }
