53af6bdc83eda88952c24ea8e1bf43a11a6e5aa9
angelasrsh <angelasrsh@gmail.com>
2024-01-21 17:52:54 -0600

    soc.cpp and .h added

--- Core/Inc/soc_estimation.h
+++ Core/Inc/soc_estimation.h
@@ -1,8 +1,13 @@
-//
-// Created by Angela Suresh on 1/14/24.
-//
+/**
+ * @brief Header for SOC Estimation
+ * @author Angela Suresh
+ * @date 1-14-24
+ *
+ */
 
 #ifndef HVC_FIRMWARE_2024_SOC_ESTIMATION_H
 #define HVC_FIRMWARE_2024_SOC_ESTIMATION_H
+
+float calculateSoc(float deltaTime);
 
 #endif //HVC_FIRMWARE_2024_SOC_ESTIMATION_H
--- Core/Src/soc_estimation.cpp
+++ Core/Src/soc_estimation.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Angela Suresh on 1/14/24.
 //
+
+
+
+#include "isense.h"
+
+float outputCurr = getPackCurrent();
+float battery_rated_capacity = 11.2244; // 6.6kWh at 588 V => 11.2244 Ah
+
+float calculateSoc(float deltaTime) {
+    float step_size = 0.003f;
+    float dt = deltaTime/step_size;
+    float soc = 0;
+    float charge = 0;
+
+    charge += outputCurr * dt;
+    soc = 100.0f + (-charge/battery_rated_capacity) * 100.0f;
+
+    return soc;
+}
+
