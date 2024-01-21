8537499ba2eb9aad4499d1bbcd3d2dee0a2fa359
angelasrsh <angelasrsh@gmail.com>
2024-01-21 17:52:08 -0600

    soc.cpp and .h added

---
+++ Core/Inc/soc_estimation.h
@@ -0,0 +1,8 @@
+//
+// Created by Angela Suresh on 1/14/24.
+//
+
+#ifndef HVC_FIRMWARE_2024_SOC_ESTIMATION_H
+#define HVC_FIRMWARE_2024_SOC_ESTIMATION_H
+
+#endif //HVC_FIRMWARE_2024_SOC_ESTIMATION_H
---
+++ Core/Src/soc_estimation.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Angela Suresh on 1/14/24.
+//
