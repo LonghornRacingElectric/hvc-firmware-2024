0c2cdb97bf8fbb1600a6f437c7a1357b0386555a
Roland Wang <rolandwang@utexas.edu>
2024-01-21 18:11:25 -0600

    tsense implementation

--- Core/Inc/tsense.h
+++ Core/Inc/tsense.h
@@ -5,4 +5,14 @@
 #ifndef HVC_FIRMWARE_2024_TSENSE_H
 #define HVC_FIRMWARE_2024_TSENSE_H
 
+#define SPI_TIMEOUT 1000 // Timeout
+
+#include <cstdint>
+
+uint8_t thermistorData[2];
+
+void tsenseInit();
+
+void tsensePeriodic();
+
 #endif //HVC_FIRMWARE_2024_TSENSE_H
--- Core/Src/tsense.cpp
+++ Core/Src/tsense.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "tsense.h"
+#include "spi.h"
+
+void tsenseInit() {
+    // TODO implement
+}
+
+void tsensePeriodic() {
+    HAL_SPI_Receive(&hspi1, thermistorData, 1, SPI_TIMEOUT);
+}
+
+uint16_t getAmbientTemp() {
+    return (uint16_t) ((thermistorData[1] << 8) || thermistorData[0]);
+}
