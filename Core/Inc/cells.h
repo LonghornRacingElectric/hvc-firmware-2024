c45fb877a6e8508af1d44e4daa9084a12b38a97a
Roland Wang <rolandwang@utexas.edu>
2024-01-23 20:22:08 -0600

    minor fix

--- Core/Inc/cells.h
+++ Core/Inc/cells.h
@@ -11,8 +11,8 @@
 static uint8_t rawData[60];       // Raw data from LTC read command
 static uint16_t voltageData[140]; // 5 segments, 28 cells per segment, 4 per LTC6813 chip, 2 bytes per voltage
 static uint16_t tempData[90];     // 9 thermistors per LTC6813 chip, 2 bytes per temp
-static float currentMinTemp = -999.0f;
-static float currentMaxTemp = 999.0f;
+static float currentMinTemp = 999.0f;
+static float currentMaxTemp = -999.0f;
 static float minTemp;
 static float maxTemp;
 static float packVoltage;
