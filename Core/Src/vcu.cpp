431532bb1f4a714b4710bbcbe721a938a61859bc
Roland Wang <rolandwang@utexas.edu>
2024-02-25 16:49:37 -0600

    vcu.cpp new can implementation

--- Core/Src/vcu.cpp
+++ Core/Src/vcu.cpp
@@ -13,7 +13,7 @@ static CanOutbox packStatus;
 static CanOutbox imuAccel;
 static CanOutbox imuGyro;
 static CanOutbox indicatorStatus;
-static CanOutbox fanRPMs; // Percentage of max fan RPM
+static CanOutbox fanRPMs;
 
 void vcuInit() {
     can_addInbox(VCU_HVC_PARAMS, &parameterInbox);
@@ -31,6 +31,35 @@ void vcuInit() {
 void vcuPeriodic(bool amsIndicator, bool imdIndicator, int state) {
 
     // Battery Pack and IMU Data
+    can_writeInt(uint16_t, &packStatus, 0, (uint16_t)(getPackVoltageFromCells() / 0.01f));
+    can_writeInt(uint16_t, &packStatus, 2, (uint16_t)(getPackCurrent() / 0.1f));
+    can_writeInt(uint8_t, &packStatus, 4, (uint8_t) getSoC());
+    can_writeInt(uint8_t, &packStatus, 5, (uint8_t) getMaxTemp());
+    can_writeInt(uint8_t, &packStatus, 6, (uint8_t) getAmbientTemp());
+
+    can_writeInt(int16_t, &imuAccel, 0, (int16_t) accelData.x);
+    can_writeInt(int16_t, &imuAccel, 2, (int16_t) accelData.y);
+    can_writeInt(int16_t, &imuAccel, 4, (int16_t) accelData.z);
+
+    can_writeInt(int16_t, &imuGyro, 0, (int16_t) gyroData.x);
+    can_writeInt(int16_t, &imuGyro, 2, (int16_t) gyroData.y);
+    can_writeInt(int16_t, &imuGyro, 4, (int16_t) gyroData.z);
+
+    can_writeInt(int16_t, &fanRPMs, 0, (int16_t) trueRpmMain);
+    can_writeInt(int16_t, &fanRPMs, 2, (int16_t) trueRpmUnique);
+
+    // Indicator Status
+    can_writeInt(uint8_t, &indicatorStatus, 0, (uint8_t) amsIndicator);
+    can_writeInt(uint8_t, &indicatorStatus, 1, (uint8_t) imdIndicator);
+
+    if(parameterInbox.isRecent) {
+        parameterInbox.isRecent = false;
+        auto minTempParam = (float) can_readInt(int8_t, &parameterInbox, 0);
+        auto maxTempParam = (float) can_readInt(int8_t, &parameterInbox, 1);
+        updateTempParameters(minTempParam, maxTempParam);
+    }
+
+    /*
     can_writeBytes(packStatus.data, 0, 1, (uint16_t) (getPackVoltageFromCells() / 0.01f));
     can_writeBytes(packStatus.data, 2, 3, (uint16_t) (getPackCurrent() / 0.1f));
     can_writeBytes(packStatus.data, 4, 4, (uint8_t) getSoC());
@@ -59,4 +88,5 @@ void vcuPeriodic(bool amsIndicator, bool imdIndicator, int state) {
         auto maxTempParam = (float) can_readBytes(parameterInbox.data, 1, 1);
         updateTempParameters(minTempParam, maxTempParam);
     }
+     */
 }
